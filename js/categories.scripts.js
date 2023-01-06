896df9532494ce768b18ca1629360c4edcd5c925
Serena Dean <dserena01@yahoo.com>
2023-01-06 00:14:38 -0400

    Implemented json into crew page

--- js/categories.scripts.js
+++ js/categories.scripts.js
@@ -1,9 +1,17 @@
 // variables
+// destination
 const destinationImage = document.querySelector(".destination-image img");
 const destinationName = document.querySelector(".destination-name");
 const destinationText = document.querySelector(".destination-text");
 const destinationDistanceKm = document.querySelector(".destination-distance-km");
 const destinationTravelTimeDays = document.querySelector(".destination-travel-time-days");
+
+// crew
+const crewImage = document.querySelector(".crew-image img");
+const crewTitle = document.querySelector(".crew-title");
+const crewName = document.querySelector(".crew-name");
+const crewText = document.querySelector(".crew-body");
+
 const currentPath = window.location.pathname.split("/").pop();
 const currentDestination = currentPath.split("-")[0];
 const currentDestinationName = currentPath.split("-")[1];
@@ -25,11 +33,19 @@ const scanData = () => {
                         }
                     })
                     break;
+                case 'crew':
+                    data[category].map(({name, images, role, bio}) => {
+                        if(role.toLowerCase().includes(currentDestinationName.split(".")[0])) {
+                            crewImage.src = images.png;
+                            crewImage.alt = name;
+                            crewName.innerHTML = name;
+                            crewTitle.innerHTML = role;
+                            crewText.innerHTML = bio;
+                        }
+                    })
+                    break;
                 case 'technology':
                     // console.log('tech');
-                    break;
-                case 'crew':
-                    // console.log('crew');
                     break;
                 default:
                     // console.log('default')
